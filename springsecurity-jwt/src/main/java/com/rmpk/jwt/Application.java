dc04f1e96534dc765259c624c69ce9c2385d1a80
pradeepkrathnayaka <pradeepkrathnayaka@gmail.com>
2020-09-07 22:03:40 +0800

    mofified for auto config

--- springsecurity-jwt/src/main/java/com/rmpk/jwt/Application.java
+++ springsecurity-jwt/src/main/java/com/rmpk/jwt/Application.java
@@ -2,8 +2,9 @@ package com.rmpk.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {HazelcastAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
